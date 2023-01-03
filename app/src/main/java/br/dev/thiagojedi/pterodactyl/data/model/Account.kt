f9ae98544bb2a115f759921b10361fa025e10b6c
Thiago Jedi <thiagoa7@gmail.com>
2023-01-03 19:29:55 -0300

    Fix serializing problems

--- app/src/main/java/br/dev/thiagojedi/pterodactyl/data/model/Account.kt
+++ app/src/main/java/br/dev/thiagojedi/pterodactyl/data/model/Account.kt
@@ -1,5 +1,8 @@
 package br.dev.thiagojedi.pterodactyl.data.model
 
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
 data class Account(
     val id: String,
     val username: String,
@@ -7,23 +10,23 @@ data class Account(
     val display_name: String,
     val locked: Boolean,
     val bot: Boolean,
-//    @SerializedName("create_at") val createdAt: Instant,
+    @SerializedName("create_at") val createdAt: Date,
     val note: String,
     val url: String,
     val avatar: String,
-//    @SerializedName("avatar_static") val avatarStatic: String,
+    @SerializedName("avatar_static") val avatarStatic: String,
     val header: String,
-//    @SerializedName("header_static") val headerStatic: String,
-//    @SerializedName("followers_count") val followersCount: Int,
-//    @SerializedName("following_count") val followingCount: Int,
-//    @SerializedName("statuses_count") val statusesCount: Int,
-//    @SerializedName("last_status_at") val lastStatusAt: Instant,
+    @SerializedName("header_static") val headerStatic: String,
+    @SerializedName("followers_count") val followersCount: Int,
+    @SerializedName("following_count") val followingCount: Int,
+    @SerializedName("statuses_count") val statusesCount: Int,
+    @SerializedName("last_status_at") val lastStatusAt: Date,
     val emojis: List<CustomEmoji>,
     val fields: List<Field>,
 ) {
     data class Field(
         val name: String,
         val value: String,
-//        @SerializedName("verified_at") val verifiedAt: Instant,
+        @SerializedName("verified_at") val verifiedAt: Date,
     )
 }
--- app/src/main/java/br/dev/thiagojedi/pterodactyl/data/model/Status.kt
+++ app/src/main/java/br/dev/thiagojedi/pterodactyl/data/model/Status.kt
@@ -1,25 +1,59 @@
 package br.dev.thiagojedi.pterodactyl.data.model
 
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
 data class Status(
     val id: String,
-//    val created_at: Instant,
-//    @SerializedName("in_reply_to_id") val inReplyToId: String,
-//    @SerializedName("in_reply_to_account_id") val inReplyToAccountId: String,
+    @SerializedName("created_at") val createdAt: Date,
+    @SerializedName("in_reply_to_id") val inReplyToId: String?,
+    @SerializedName("in_reply_to_account_id") val inReplyToAccountId: String?,
     val sensitive: Boolean,
-//    @SerializedName("spoiler_text") val spoilerText: String,
+    @SerializedName("spoiler_text") val spoilerText: String,
     val visibility: String,
     val language: String,
     val uri: String,
     val url: String,
-//    @SerializedName("replies_count") val repliesCount: Int,
-//    @SerializedName("reblogs_count") val reblogsCount: Int,
-//    @SerializedName("favourites_count") val favouritesCount: Int,
+    @SerializedName("replies_count") val repliesCount: Int,
+    @SerializedName("reblogs_count") val reblogsCount: Int,
+    @SerializedName("favourites_count") val favouritesCount: Int,
     val favourited: Boolean,
     val reblogged: Boolean,
     val muted: Boolean,
     val bookmarked: Boolean,
     val content: String,
     val account: Account,
-    val emojis: List<CustomEmoji>
-)
+    val emojis: List<CustomEmoji>,
+    @SerializedName("media_attachments") val mediaAttachments: List<MediaAttachment>
+) {
+    enum class MediaAttachmentType {
+        @SerializedName("unknown")
+        UNKNOWN,
 
+        @SerializedName("image")
+        IMAGE,
+
+        @SerializedName("gifv")
+        GIF,
+
+        @SerializedName("video")
+        VIDEO,
+
+        @SerializedName("audio")
+        AUDIO
+    }
+
+    data class MediaAttachment(
+        val id: String,
+        val type: MediaAttachmentType,
+        val url: String,
+        @SerializedName("preview_url") val previewUrl: String,
+        @SerializedName("remote_url") val remoteUrl: String?,
+        val description: String?,
+        @SerializedName("blurhash") val blurHash: String,
+        val meta: ImageMeta,
+    ) {
+        data class MetaSize(val width: Int, val height: Int)
+        data class ImageMeta(val original: MetaSize?, val small: MetaSize?)
+    }
+}
