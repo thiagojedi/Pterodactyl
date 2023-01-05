37840595b1acbe160955a2bb23a93d247be23c30
Thiago Jedi <thiagoa7@gmail.com>
2023-01-05 18:19:54 -0300

    Update fab

--- app/src/main/java/br/dev/thiagojedi/pterodactyl/ui/components/TootFab.kt
+++ app/src/main/java/br/dev/thiagojedi/pterodactyl/ui/components/TootFab.kt
@@ -1,23 +1,39 @@
 package br.dev.thiagojedi.pterodactyl.ui.components
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun TootFab() {
-    FloatingActionButton(
-            onClick = { /*TODO*/ },
-    ) {
-        Icon(Icons.Filled.Create, contentDescription = "Toot")
-    }
+fun TootFab(text: String = "Toot") {
+    val (value, setValue) = remember { mutableStateOf(text) }
+
+    ExtendedFloatingActionButton(
+        onClick = { setValue("Aqui") },
+        icon = { Icon(Icons.Outlined.Create, "Localized description") },
+        text = { Text(text = value) },
+    )
 }
 
 @Preview
 @Composable
 fun TootFabPreview() {
-    TootFab()
+    val mock = "Postar"
+
+    TootFab(mock)
 }
+
+@Preview
+@Composable
+fun TootFabPreviewDois() {
+    val mock = "Escrever"
+
+    TootFab(mock)
+}
+
