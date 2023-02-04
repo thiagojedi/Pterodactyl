24c630074e6d42182a2bbe8b89b8bf33fa278b62
Thiago Jedi <thiagoa7@gmail.com>
2023-02-04 19:24:45 -0300

    Better handling datastore in base view model

--- app/src/main/java/br/dev/thiagojedi/pterodactyl/ui/viewModel/PteroViewModel.kt
+++ app/src/main/java/br/dev/thiagojedi/pterodactyl/ui/viewModel/PteroViewModel.kt
@@ -2,19 +2,18 @@ package br.dev.thiagojedi.pterodactyl.ui.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import br.dev.thiagojedi.pterodactyl.data.services.MastodonApiService
 import br.dev.thiagojedi.pterodactyl.data.services.RetrofitHelper
 import br.dev.thiagojedi.pterodactyl.data.store.AppStore
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 abstract class PteroViewModel(context: Application) : AndroidViewModel(context) {
     protected lateinit var api: MastodonApiService
     private val store: AppStore = AppStore(context)
 
     init {
-        viewModelScope.launch {
+        runBlocking {
             val baseUrl = store.getBaseUrl.first()
             val userToken = store.getUserToken.first()
 
